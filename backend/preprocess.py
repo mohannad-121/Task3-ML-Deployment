56816cbb48c0bb01036c9fffce202edd3afd5dfc
Mohannad <mabuayyash33@gmail.com>
2025-11-25 21:16:43 +0300

    Update preprocess.py
    just to reduce model.pkl size from 274.84MB to 55.09MB

--- backend/preprocess.py
+++ backend/preprocess.py
@@ -76,7 +76,8 @@ def build_model_pipeline() -> Pipeline:
     )
 
     model = RandomForestRegressor(
-        n_estimators=200,
+        n_estimators=50,
+        max_depth=20,
         random_state=42,
         n_jobs=-1,
     )
